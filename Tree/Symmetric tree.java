fb4fbae2f5d81ebd22f7aad5cb0658fa5f4a3ee0
Hemadharani <hemadharanisivakumar456@gmail.com>
2026-03-12 23:11:36 +0530

    Create Symmetric tree.java
    feat(tree): solve LC 101 Symmetric Tree using recursive mirror traversal
    
    - Compared left and right subtrees recursively
    - Checked mirror conditions (left-left with right-right and left-right with right-left)
    - Time Complexity: O(n)
    - Space Complexity: O(h)

---
+++ Tree/Symmetric tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null)
+            return true;
+            return traversal(root.left,root.right);
+        
+    }
+    boolean traversal(TreeNode root1,TreeNode root2){
+        if(root1==null&&root2==null)return true;
+        if(root1==null||root2==null)return false;
+
+        return(root1.val==root2.val)&&traversal(root1.left,root2.right)&&traversal(root1.right,root2.left);
+    }
+}
