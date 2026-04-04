3ce9da3982316bda4e58f413e35970ef6572aec9
Hemadharani <hemadharanisivakumar456@gmail.com>
2026-04-04 22:32:31 +0530

    Create Word break.java
    Implemented dynamic programming solution for word break problem
    
    - Converted word dictionary into HashSet for fast lookup
    - Defined dp[i] as true if substring s[0...i-1] can be segmented
    - Initialized dp[0] = true for empty string
    - Iterated over all possible partitions using two loops
    - Checked valid split using dp[j] and dictionary lookup
    - Broke early when valid segmentation found

---
+++ DP/Word break.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> a=new HashSet<>(wordDict);
+        boolean [] dp=new boolean[s.length()+1];
+        dp[0]=true;
+        for(int i=1;i<=s.length();i++){
+            for(int j=0;j<i;j++){
+                if(dp[j]&&a.contains(s.substring(j,i))){
+                dp[i]=true;
+                break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
